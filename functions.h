72e416c76f6a121841af4d84d2e1399a10fe04b3
Tersou <tersou.robin@gmail.com>
2017-11-12 16:09:33 +0100

    functions files created

---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions/main_func.c"
+#include "functions/string_func.c"
+#include "functions/display_func.c"
+#include "functions/calc_func.c"
+
+#endif // FUNCTIONS_H_INCLUDED
